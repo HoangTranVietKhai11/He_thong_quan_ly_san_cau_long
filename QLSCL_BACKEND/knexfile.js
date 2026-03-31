f9a9e4bbc0325e38ceb764d8846d1d711b47ee90
hoangtranvietkhai11 <HoangTranVietKhai@gmail.com>
2026-03-31 23:16:20 +0700

    Fix: Add correct seeds directory to production in knexfile.js to fix deploy error

--- QLSCL_BACKEND/knexfile.js
+++ QLSCL_BACKEND/knexfile.js
@@ -37,6 +37,9 @@ module.exports = {
     migrations: {
       directory: './db/migrations',
       tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './db/seeds'
     }
   }
-};
+};
